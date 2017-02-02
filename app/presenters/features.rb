e42a6c9c1c2723468a994c2133cfc8d17288995a
Sage Ross <ragesoss@gmail.com>
2017-02-02 10:03:44 -0800

    Add feature toggle for enabling chat on individual courses

--- app/presenters/features.rb
+++ app/presenters/features.rb
@@ -60,7 +60,14 @@ class Features
     Rails.env != 'production'
   end
 
+  # Determines whether chat is available at all within the dashboard
   def self.enable_chat?
     ENV['enable_chat'] == 'true'
+  end
+
+  # Determines whether chat is enabled for an individual course
+  def self.enable_course_chat?(course)
+    return false unless enable_chat?
+    course.flags[:enable_chat] == true
   end
 end
